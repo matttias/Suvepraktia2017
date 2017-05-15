d5140b5145d60189cb14787315e8276dfc072fc1
Mattias Blehner <matttias@hithub.com>
2017-05-15 16:02:15 +0300

    Esimene test maatriksite suuruse määramisel

---
+++ projekt.js
@@ -0,0 +1,28 @@
+/*function dropdownFunction() {
+    document.getElementById("Ydropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdownbtn')) {
+    var Ydropdowns = document.getElementsByClassName("YdropdownContent");
+    var i;
+    for (i=0; i<Ydropdowns.length; i++) {
+      var openDropdown = Ydropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+	var e = document.getElementById("dropdown");
+	var strUser = e.options[e.selectedIndex].text;
+	console.log(strUser);
+}
+*/
+//Välja printimiseks
+function ConfrmSelect() {
+    var x = document.getElementById("XSelect").value;
+    document.getElementById("test").innerHTML = x;
+	
+	var y = document.getElementById("YSelect").value;
+    document.getElementById("test2").innerHTML = y;
+}
